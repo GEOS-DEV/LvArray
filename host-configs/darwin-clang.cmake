207f56b61d7e33fb19c9fdb128a64e078ebe8be6
Randolph R. Settgast <rrsettgast@gmail.com>
2016-08-03 00:50:50 -0500

    cmake changes for cxx-utilities

---
+++ host-configs/darwin-clang.cmake
@@ -0,0 +1,13 @@
+
+set(RAJA_DIR "/Users/rrsettgast/Codes/geosx/RAJA/install-clang-release" CACHE PATH "")
+
+include("${CMAKE_CURRENT_LIST_DIR}/hc-defaults.cmake")
+
+#set(GEOSX_LINK_PREPEND_FLAG "-Wl,-force_load" CACHE PATH "" FORCE)
+#set(GEOSX_LINK_POSTPEND_FLAG "" CACHE PATH "" FORCE)
+
+
+#set(ENABLE_MPI ON CACHE PATH "")
+#set(MPI_C_COMPILER       "/opt/local/bin/mpicxx-openmpi-clang37" CACHE PATH "" FORCE)
+#set(MPI_CXX_COMPILER     "/opt/local/bin/mpicxx-openmpi-clang37" CACHE PATH "" FORCE)
+#set(MPI_Fortran_COMPILER "/opt/local/bin/mpifort-openmpi-clang37" CACHE PATH "" FORCE)
