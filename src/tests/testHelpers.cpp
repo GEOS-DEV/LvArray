c291f5e253cf9a06345d656bc69787a5a9bda728
Randolph R. Settgast <settgast1@llnl.gov>
2018-12-10 21:07:12 -0800

    fixed issue with threaded death tests in gtest

--- src/tests/testHelpers.cpp
+++ src/tests/testHelpers.cpp
@@ -64,27 +64,27 @@ void indexChecker( int const * const dims, INDICES... indices )
 
 TEST( IndexChecker, IndexChecker )
 {
-  int dims[4] = { 3, 2, 5, 3 };
+  int dims[4] = { 3, 2, 1, 3 };
+  ::testing::FLAGS_gtest_death_test_style = "threadsafe";
 
-  for( int a0=-1 ; a0<=dims[0] ; ++a0 )
+  EXPECT_DEATH_IF_SUPPORTED( indexChecker<4>( dims, -1,0,0,0 ), "" );
+  EXPECT_DEATH_IF_SUPPORTED( indexChecker<4>( dims, 0,-1,0,0 ), "" );
+  EXPECT_DEATH_IF_SUPPORTED( indexChecker<4>( dims, 0,0,-1,0 ), "" );
+  EXPECT_DEATH_IF_SUPPORTED( indexChecker<4>( dims, 0,0,0,-1 ), "" );
+  EXPECT_DEATH_IF_SUPPORTED( indexChecker<4>( dims, dims[0],0,0,0 ), "" );
+  EXPECT_DEATH_IF_SUPPORTED( indexChecker<4>( dims, 0,dims[1],0,0 ), "" );
+  EXPECT_DEATH_IF_SUPPORTED( indexChecker<4>( dims, 0,0,dims[2],0 ), "" );
+  EXPECT_DEATH_IF_SUPPORTED( indexChecker<4>( dims, 0,0,0,dims[3]), "" );
+
+  for( int a0=0 ; a0<dims[0] ; ++a0 )
   {
-    for( int a1=-1 ; a1<=dims[1] ; ++a1 )
+    for( int a1=0 ; a1<dims[1] ; ++a1 )
     {
-      for( int a2=-1 ; a2<=dims[2] ; ++a2 )
+      for( int a2=0 ; a2<dims[2] ; ++a2 )
       {
-        for( int a3=-1 ; a3<=dims[3] ; ++a3 )
+        for( int a3=0 ; a3<dims[3] ; ++a3 )
         {
-          if( a0 < 0 || a0 == dims[0] ||
-              a1 < 0 || a1 == dims[1] ||
-              a2 < 0 || a2 == dims[2] ||
-              a3 < 0 || a3 == dims[3] )
-          {
-            EXPECT_DEATH_IF_SUPPORTED( indexChecker<4>( dims, a0,a1,a2,a3 ), "" );
-          }
-          else
-          {
-            ASSERT_NO_FATAL_FAILURE( indexChecker<4>( dims, a0,a1,a2,a3 ) );
-          }
+          ASSERT_NO_FATAL_FAILURE( indexChecker<4>( dims, a0,a1,a2,a3 ) );
         }
       }
     }
@@ -100,10 +100,6 @@ TEST( StrideHelper, StrideHelper )
   int const result1 = stride_helper<3,int>::evaluate(dims+1);
   int const result2 = stride_helper<2,int>::evaluate(dims+2);
   int const result3 = stride_helper<1,int>::evaluate(dims+3);
-
-  std::cout<<dims[1]*dims[2]*dims[3]<<", "<<
-                     dims[2]*dims[3]<<", "<<
-                             dims[3]<<std::endl;
 
   ASSERT_TRUE( result0 == dims[1]*dims[2]*dims[3] );
   ASSERT_TRUE( result1 ==         dims[2]*dims[3] );
