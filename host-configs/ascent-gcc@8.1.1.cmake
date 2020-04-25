d23742d73387635db197a8c95401f600eb5681b0
Sergey Klevtsov <klevtsov@stanford.edu>
2020-04-25 16:34:16 -0400

    Update Ascent host-config

--- host-configs/ascent-gcc@8.1.1.cmake
+++ host-configs/ascent-gcc@8.1.1.cmake
@@ -29,7 +29,7 @@ set(CMAKE_Fortran_FLAGS_RELEASE "-O3 -DNDEBUG -mcpu=power9 -mtune=power9" CACHE 
 
 # MPI options
 set(ENABLE_MPI ON CACHE BOOL "")
-set(MPI_ROOT "/autofs/nccsopen-svm1_sw/ascent/.swci/1-compute/opt/spack/20180914/linux-rhel7-ppc64le/gcc-8.1.1/spectrum-mpi-10.3.0.0-20190419-juwhiawazufuuoastohlsuewaklwmazi" CACHE PATH "")
+set(MPI_ROOT "/autofs/nccsopen-svm1_sw/ascent/.swci/1-compute/opt/spack/20180914/linux-rhel7-ppc64le/gcc-8.1.1/spectrum-mpi-10.3.1.2-20200121-chae23sgwacfeot7vxkpfboz6wao2c33" CACHE PATH "")
 set(MPI_C_COMPILER         "${MPI_ROOT}/bin/mpicc"   CACHE PATH "")
 set(MPI_CXX_COMPILER       "${MPI_ROOT}/bin/mpicxx"  CACHE PATH "")
 set(MPI_Fortran_COMPILER   "${MPI_ROOT}/bin/mpifort" CACHE PATH "")
@@ -39,12 +39,13 @@ set(ENABLE_WRAP_ALL_TESTS_WITH_MPIEXEC ON CACHE BOOL "")
 
 # Cuda options
 set(ENABLE_CUDA ON CACHE BOOL "")
-set(CUDA_TOOLKIT_ROOT_DIR "/sw/ascent/cuda/10.1.105" CACHE STRING "")
+set(CUDA_TOOLKIT_ROOT_DIR "/sw/ascent/cuda/10.1.243" CACHE STRING "")
 set(CMAKE_CUDA_HOST_COMPILER ${MPI_CXX_COMPILER} CACHE STRING "")
-set(CMAKE_CUDA_COMPILER ${CUDA_TOOLKIT_ROOT_DIR}/bin/nvcc CACHE STRING "")
+set(CMAKE_CUDA_COMPILER "${CUDA_TOOLKIT_ROOT_DIR}/bin/nvcc" CACHE STRING "")
 set(CUDA_ARCH sm_70 CACHE STRING "")
 set(CMAKE_CUDA_STANDARD 14 CACHE STRING "")
-set(CMAKE_CUDA_FLAGS "-restrict -arch ${CUDA_ARCH} --expt-extended-lambda --expt-relaxed-constexpr -Werror cross-execution-space-call,reorder,deprecated-declarations" CACHE STRING "")
+# -std=c++14 needed to work around cuda/gcc bug
+set(CMAKE_CUDA_FLAGS "-std=c++14 -Xcompiler -mno-float128 -restrict -arch ${CUDA_ARCH} --expt-extended-lambda --expt-relaxed-constexpr -Werror cross-execution-space-call,reorder,deprecated-declarations" CACHE STRING "")
 set(CMAKE_CUDA_FLAGS_RELEASE "-O3 -DNDEBUG -Xcompiler -DNDEBUG -Xcompiler -O3 -Xcompiler -mcpu=powerpc64le -Xcompiler -mtune=powerpc64le" CACHE STRING "")
 set(CMAKE_CUDA_FLAGS_RELWITHDEBINFO "-g -lineinfo ${CMAKE_CUDA_FLAGS_RELEASE}" CACHE STRING "")
 set(CMAKE_CUDA_FLAGS_DEBUG "-g -G -O0 -Xcompiler -O0" CACHE STRING "")
