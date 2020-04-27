6776439935272aa7e57de8364b0087906a326d3b
Sergey Klevtsov <klevtsov@stanford.edu>
2020-04-27 12:02:51 -0400

    Update TPL location

--- host-configs/ascent-gcc@8.1.1.cmake
+++ host-configs/ascent-gcc@8.1.1.cmake
@@ -1,7 +1,7 @@
 set(CONFIG_NAME "ascent-gcc@8.1.1" CACHE PATH "") 
 
 # Set up the tpls
-set(GEOSX_TPL_ROOT_DIR "/ccsopen/home/klevtsov/thirdPartyLibs" CACHE PATH "")
+set(GEOSX_TPL_ROOT_DIR "/ccsopen/proj/gen136/GEOSX/thirdPartyLibs" CACHE PATH "")
 set(GEOSX_TPL_DIR ${GEOSX_TPL_ROOT_DIR}/install-${CONFIG_NAME}-release CACHE PATH "")
 
 # C options
