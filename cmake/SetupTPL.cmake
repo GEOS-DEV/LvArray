6345984597d64896450a59b944e1f78f87564609
Randolph R. Settgast <settgast1@llnl.gov>
2020-05-05 15:42:27 -0700

    add caliper/adiak to tpls

--- cmake/SetupTPL.cmake
+++ cmake/SetupTPL.cmake
@@ -31,4 +31,51 @@ set(ENABLE_RAJA ON CACHE BOOL "")
 
 set( thirdPartyLibs ${thirdPartyLibs} raja )
 
+
+################################
+# CALIPER and Adiak
+################################
+if(ENABLE_CALIPER)
+    if(NOT EXISTS ${ADIAK_DIR})
+        set(ADIAK_DIR ${GEOSX_TPL_DIR}/adiak)
+    endif()
+
+    message(STATUS "Using adiak at ${ADIAK_DIR}")
+
+    find_package(adiak REQUIRED
+                 PATHS ${ADIAK_DIR}/lib/cmake/adiak)
+
+    blt_register_library(NAME adiak
+                         INCLUDES ${adiak_INCLUDE_DIRS}
+                         LIBRARIES ${adiak_LIBRARIES}
+                         TREAT_INCLUDES_AS_SYSTEM ON)
+
+    set(thirdPartyLibs ${thirdPartyLibs} adiak)
+
+    if(NOT EXISTS ${CALIPER_DIR})
+        set(CALIPER_DIR ${GEOSX_TPL_DIR}/caliper)
+    endif()
+
+    message(STATUS "Using caliper at ${CALIPER_DIR}")
+
+    find_package(caliper REQUIRED
+                 PATHS ${CALIPER_DIR}/share/cmake/caliper)
+ 
+    if(ENABLE_MPI)
+        set(caliper_LIBRARIES caliper-mpi)
+    else()
+        set(caliper_LIBRARIES caliper)
+    endif()
+
+    blt_register_library(NAME caliper
+                         INCLUDES ${caliper_INCLUDE_PATH}
+                         LIBRARIES ${caliper_LIBRARIES}
+                         TREAT_INCLUDES_AS_SYSTEM ON)
+
+    set(thirdPartyLibs ${thirdPartyLibs} caliper)
+endif()
+
+
+
+
 set( thirdPartyLibs ${thirdPartyLibs} CACHE STRING "" )
