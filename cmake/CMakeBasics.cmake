ed8cd28c990c136370d33ba9c6dfbb81bd5c6a0f
Randolph R. Settgast <settgast1@llnl.gov>
2021-04-08 20:18:52 -0700

    make LvArrayBuildAsObj an option

--- cmake/CMakeBasics.cmake
+++ cmake/CMakeBasics.cmake
@@ -9,6 +9,8 @@ endif()
 
 option( ENABLE_TOTALVIEW_OUTPUT "" OFF )
 
+option( LvArrayBuildAsObj "" OFF )
+
 
 if( NOT BLT_CXX_STD STREQUAL c++14 )
     MESSAGE(FATAL_ERROR "c++14 is NOT enabled. LvArray requires c++14")
