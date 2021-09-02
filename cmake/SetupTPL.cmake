7b9e4e3f9928e3dad74315d3db089587be88b058
wrtobin <tobin6@llnl.gov>
2021-09-02 09:36:17 -0700

    reverting some temp cmake changes

--- cmake/SetupTPL.cmake
+++ cmake/SetupTPL.cmake
@@ -42,11 +42,11 @@ if(ENABLE_UMPIRE)
 
     find_package(umpire REQUIRED
                  PATHS ${UMPIRE_DIR})
-    
-    # blt_register_library(NAME umpire
-    #                      INCLUDES ${UMPIRE_INCLUDE_DIRS}
-    #                      LIBRARIES umpire
-    #                      TREAT_INCLUDES_AS_SYSTEM ON)
+
+    blt_register_library(NAME umpire
+                         INCLUDES ${UMPIRE_INCLUDE_DIRS}
+                         LIBRARIES umpire
+                         TREAT_INCLUDES_AS_SYSTEM ON)
 
     set(thirdPartyLibs ${thirdPartyLibs} umpire)
 else()
@@ -74,11 +74,11 @@ if(ENABLE_CHAI)
     find_package(chai REQUIRED
                  PATHS ${CHAI_DIR})
 
-    # # If this isn't done chai will add -lRAJA to the link line, but we don't link to RAJA like that.
-    # get_target_property(CHAI_LINK_LIBRARIES chai INTERFACE_LINK_LIBRARIES)
-    # list(REMOVE_ITEM CHAI_LINK_LIBRARIES RAJA)
-    # set_target_properties(chai
-    #                       PROPERTIES INTERFACE_LINK_LIBRARIES "${CHAI_LINK_LIBRARIES}")
+    # If this isn't done chai will add -lRAJA to the link line, but we don't link to RAJA like that.
+    get_target_property(CHAI_LINK_LIBRARIES chai INTERFACE_LINK_LIBRARIES)
+    list(REMOVE_ITEM CHAI_LINK_LIBRARIES RAJA)
+    set_target_properties(chai
+                          PROPERTIES INTERFACE_LINK_LIBRARIES "${CHAI_LINK_LIBRARIES}")
 
     set(thirdPartyLibs ${thirdPartyLibs} chai)
 else()
@@ -96,13 +96,15 @@ if(ENABLE_CALIPER)
 
     message(STATUS "Using caliper from ${CALIPER_DIR}")
 
+    set(FIND_LIBRARY_USE_LIB64_PATHS TRUE)
+
     find_package(caliper REQUIRED
                  PATHS ${CALIPER_DIR})
 
-    # blt_register_library(NAME caliper
-    #                      INCLUDES ${caliper_INCLUDE_PATH}
-    #                      LIBRARIES caliper
-    #                      TREAT_INCLUDES_AS_SYSTEM ON)
+    blt_register_library(NAME caliper
+                         INCLUDES ${caliper_INCLUDE_PATH}
+                         LIBRARIES caliper
+                         TREAT_INCLUDES_AS_SYSTEM ON)
 
     set(thirdPartyLibs ${thirdPartyLibs} caliper)
 else()
