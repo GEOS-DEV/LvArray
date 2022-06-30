2fcfa6816fb928ba400e4c937ac4b243cd9fcf14
wrtobin <tobin6@llnl.gov>
2022-06-30 10:35:03 -0700

    better raja scan version guard

--- src/ArrayOfArraysView.hpp
+++ src/ArrayOfArraysView.hpp
@@ -732,7 +732,7 @@ protected:
     auto const fillOffsets = [&]()
     {
       m_offsets[ 0 ] = 0;
-#if RAJA_VERSION_MAJOR >= 1 && RAJA_VERSION_MINOR >= 13
+#if ( RAJA_VERSION_MAJOR == 1 && RAJA_VERSION_MINOR >= 13 ) || ( RAJA_VERSION_MAJOR > 1 )
       RAJA::inclusive_scan< POLICY >( RAJA::make_span( capacities, numSubArrays ), RAJA::make_span( m_offsets.data() + 1, numSubArrays ) );
 #else
       RAJA::inclusive_scan< POLICY >( capacities, capacities + numSubArrays, m_offsets.data() + 1 );
