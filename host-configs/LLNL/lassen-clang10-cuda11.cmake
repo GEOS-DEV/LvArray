dc06f3b0c2f0cdbf646afd170b24d1d82e952978
Randolph Settgast <settgast1@llnl.gov>
2022-11-16 01:08:18 -0800

    update LC tpl paths

--- host-configs/LLNL/lassen-clang10-cuda11.cmake
+++ host-configs/LLNL/lassen-clang10-cuda11.cmake
@@ -14,7 +14,7 @@ set(CMAKE_CXX_FLAGS_DEBUG "-O0 -g" CACHE STRING "")
 
 # Set up the tpls
 set(GEOSX_TPL_ROOT_DIR /usr/gapps/GEOSX/thirdPartyLibs CACHE PATH "")
-set(GEOSX_TPL_DIR ${GEOSX_TPL_ROOT_DIR}/2022-11-07/install-${CONFIG_NAME}-release CACHE PATH "")
+set(GEOSX_TPL_DIR ${GEOSX_TPL_ROOT_DIR}/2022-11-12/install-${CONFIG_NAME}-release CACHE PATH "")
 
 set(CAMP_DIR ${GEOSX_TPL_DIR}/raja CACHE PATH "")
 set(RAJA_DIR ${GEOSX_TPL_DIR}/raja CACHE PATH "")
