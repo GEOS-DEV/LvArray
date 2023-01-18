385f18bf63135ebd2f1a5133ef5877f88920d8c2
Yohann Dudouit <yohann.dudouit@gmail.com>
2023-01-18 08:32:05 -0800

    Add host configs for tioga.

---
+++ host-configs/LLNL/tioga-cce@14.0.1.cmake
@@ -0,0 +1,38 @@
+
+set(CONFIG_NAME "tioga-cce@14.0.1" CACHE PATH "")
+include( ${CMAKE_CURRENT_LIST_DIR}/tioga-base.cmake )
+
+# Set up the tpls
+set(GEOSX_TPL_DIR "/usr/workspace/dudouit1/spack/opt/spack/linux-rhel8-x86_64/cce-14.0.1" CACHE PATH "")
+
+set(CAMP_DIR "${GEOSX_TPL_DIR}/camp-2022.03.2-uzyab2oftavddnkf6pqnjhogfeie6zw3" CACHE PATH "" )
+
+set(RAJA_DIR "${GEOSX_TPL_DIR}/raja-2022.03.0-wkr76cympyfdh5zt5elss6vocbtqq3wy" CACHE PATH "" )
+
+set(ENABLE_UMPIRE TRUE CACHE BOOL "" )
+set(UMPIRE_DIR "${GEOSX_TPL_DIR}/umpire-2022.03.0-4a76dy3xxyr7c574v6cthu6olo6djsnf" CACHE PATH "" )
+
+set(ENABLE_CHAI TRUE CACHE BOOL "" )
+set(CHAI_DIR "${GEOSX_TPL_DIR}/chai-2022.03.0-wzou726pxbpr5ll7koufjzxwda7yjqcz" CACHE PATH "" )
+
+set(METIS_DIR "${GEOSX_TPL_DIR}/metis-5.1.0-5hcqp42kkq3njvpw25f45l2jikaecofm" CACHE PATH "" )
+set(PARMETIS_DIR "${GEOSX_TPL_DIR}/parmetis-4.0.3-4duubemdbsj3oipdf3s3vvce2aeegzlb" CACHE PATH "" )
+
+# C++ options
+set(CRAYPE_VERSION "2.7.19")
+set(CMAKE_C_COMPILER "/opt/cray/pe/craype/${CRAYPE_VERSION}/bin/cc" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "/opt/cray/pe/craype/${CRAYPE_VERSION}/bin/CC" CACHE PATH "")
+set(CMAKE_Fortran_COMPILER "/opt/cray/pe/craype/${CRAYPE_VERSION}/bin/ftn" CACHE PATH "")
+
+if( ENABLE_HIP )
+  set( ENABLE_CLANG_HIP ON CACHE BOOL "" FORCE ) # don't invoke hipcc, rely on cce link-time compilation
+
+  set( HIP_VERSION_STRING "5.1.0" CACHE STRING "" )
+  set( HIP_ROOT "/opt/rocm-${HIP_VERSION_STRING}" CACHE PATH "" )
+  set( hip_DIR "/opt/rocm-${HIP_VERSION_STRING}" CACHE PATH "" )
+
+  set( CMAKE_HIP_ARCHITECTURES "gfx90a" CACHE STRING "" FORCE )
+  set( AMDGPU_TARGETS "${CMAKE_HIP_ARCHITECTURES}" CACHE STRING "" FORCE )
+  set( CMAKE_CXX_FLAGS "-mno-unsafe-fp-atomics -fgpu-rdc" CACHE STRING "" FORCE )
+  set( CMAKE_CXX_LINK_FLAGS "-fgpu-rdc --hip-link" CACHE STRING "" FORCE )
+endif()
