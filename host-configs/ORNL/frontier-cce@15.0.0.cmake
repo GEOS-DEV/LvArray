d221366dd4ce3aca14f55d881db546b5c4a40656
wrtobin <tobin6@llnl.gov>
2023-04-18 09:34:07 -0700

    removing old host-configs, only retain the current working frontier host-config (should also work on crusher)

--- host-configs/ORNL/frontier-cce@15.0.0.cmake
+++ host-configs/ORNL/frontier-cce@15.0.0.cmake
@@ -1,22 +1,23 @@
-include( ${CMAKE_CURRENT_LIST_DIR}/crusher-base.cmake )
+include( ${CMAKE_CURRENT_LIST_DIR}/frontier-base.cmake )
 
 set(CCE_VERSION 15.0.0)
-set(CONFIG_NAME "crusher-cce@${CCE_VERSION}" CACHE PATH "")
+set(CONFIG_NAME "frontier-cce@${CCE_VERSION}" CACHE PATH "")
 
 # Set up the tpls
-set(GEOSX_TPL_DIR "/gpfs/alpine/geo127/world-shared/spack/opt/2023-02-17/linux-sles15-zen3/cce-${CCE_VERSION}" CACHE PATH "")
+set( GEOSX_TPL_DATE 2023-04-04 )
+set( GEOSX_TPL_DIR "/lustre/orion/geo127/world-shared/tpls/${GEOSX_TPL_DATE}/install-${CONFIG_NAME}/" CACHE PATH "")
 
-set(CAMP_DIR "${GEOSX_TPL_DIR}/camp" CACHE PATH "" )
-set(RAJA_DIR "${GEOSX_TPL_DIR}/raja" CACHE PATH "" )
+set(CAMP_DIR "${GEOSX_TPL_DIR}/camp-2022.03.2" CACHE PATH "" )
+set(RAJA_DIR "${GEOSX_TPL_DIR}/raja-2022.03.0" CACHE PATH "" )
 
 set(ENABLE_UMPIRE TRUE CACHE BOOL "" )
-set(UMPIRE_DIR "${GEOSX_TPL_DIR}/umpire" CACHE PATH "" )
+set(UMPIRE_DIR "${GEOSX_TPL_DIR}/umpire-2022.03.0" CACHE PATH "" )
 
 set(ENABLE_CHAI TRUE CACHE BOOL "" )
-set(CHAI_DIR "${GEOSX_TPL_DIR}/chai" CACHE PATH "" )
+set(CHAI_DIR "${GEOSX_TPL_DIR}/chai-2022.03.0" CACHE PATH "" )
 
-set(METIS_DIR "${GEOSX_TPL_DIR}/metis" CACHE PATH "" )
-set(PARMETIS_DIR "${GEOSX_TPL_DIR}/parmetis" CACHE PATH "" )
+set(METIS_DIR "${GEOSX_TPL_DIR}/metis-5.1.0" CACHE PATH "" )
+set(PARMETIS_DIR "${GEOSX_TPL_DIR}/parmetis-4.0.3" CACHE PATH "" )
 
 # C++ options
 set(CRAYPE_VERSION "2.7.16")
@@ -25,7 +26,7 @@ set(CMAKE_CXX_COMPILER "/opt/cray/pe/craype/${CRAYPE_VERSION}/bin/CC" CACHE PATH
 set(CMAKE_Fortran_COMPILER "/opt/cray/pe/craype/${CRAYPE_VERSION}/bin/ftn" CACHE PATH "")
 
 if( ENABLE_HIP )
-  set( ROCM_VERSION 5.4.0 CACHE STRING "" )
+  set( ROCM_VERSION 5.4.3 CACHE STRING "" )
   set( HIP_ROOT "/opt/rocm-${ROCM_VERSION}" CACHE PATH "" )
 
   set( CMAKE_HIP_ARCHITECTURES "gfx90a" CACHE STRING "" FORCE )
