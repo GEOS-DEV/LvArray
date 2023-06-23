ca662ae0c1483229fea5e8e53f573e8b14fa566e
Matteo Cusini <matteo.cusini@gmail.com>
2023-06-23 08:37:38 -0700

    cuda 11.8.0

--- host-configs/LLNL/lassen-cuda-11-base.cmake
+++ host-configs/LLNL/lassen-cuda-11-base.cmake
@@ -2,11 +2,11 @@ include(${CMAKE_CURRENT_LIST_DIR}/lassen-base.cmake)
 
 # Cuda options
 set(ENABLE_CUDA ON CACHE BOOL "")
-set(CUDA_TOOLKIT_ROOT_DIR /usr/tce/packages/cuda/cuda-11.2.0 CACHE STRING "")
+set(CUDA_TOOLKIT_ROOT_DIR /usr/tce/packages/cuda/cuda-11.8.0 CACHE STRING "")
 set(CMAKE_CUDA_HOST_COMPILER ${CMAKE_CXX_COMPILER} CACHE STRING "")
 set(CMAKE_CUDA_COMPILER ${CUDA_TOOLKIT_ROOT_DIR}/bin/nvcc CACHE STRING "")
 set(CUDA_ARCHITECTURES sm_70 CACHE STRING "")
-set(CMAKE_CUDA_STANDARD 14 CACHE STRING "")
+set(CMAKE_CUDA_STANDARD 17 CACHE STRING "")
 set(CMAKE_CUDA_FLAGS "-restrict -arch ${CUDA_ARCHITECTURES} --expt-extended-lambda -Werror cross-execution-space-call,reorder,deprecated-declarations" CACHE STRING "")
 set(CMAKE_CUDA_FLAGS_RELEASE "-O3 -DNDEBUG -Xcompiler -DNDEBUG -Xcompiler -O3 -Xcompiler -mcpu=powerpc64le -Xcompiler -mtune=powerpc64le" CACHE STRING "")
 set(CMAKE_CUDA_FLAGS_RELWITHDEBINFO "-g -lineinfo ${CMAKE_CUDA_FLAGS_RELEASE}" CACHE STRING "")
