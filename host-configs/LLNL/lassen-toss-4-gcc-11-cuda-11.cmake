d2eca999a25a4254c2d58fd6f4225ea39856786c
Brian Han <han12@llnl.gov>
2023-09-13 11:35:38 -0700

    gcc > clang

--- host-configs/LLNL/lassen-toss-4-gcc-11-cuda-11.cmake
+++ host-configs/LLNL/lassen-toss-4-gcc-11-cuda-11.cmake
@@ -1,8 +1,8 @@
 set(CONFIG_NAME "lassen-toss-4-gcc-11-cuda-11" CACHE PATH "")
 
 set(COMPILER_DIR  /usr/tce/packages/gcc/gcc-11.2.1)
-set(CMAKE_C_COMPILER ${COMPILER_DIR}/bin/clang CACHE PATH "")
-set(CMAKE_CXX_COMPILER ${COMPILER_DIR}/bin/clang++ CACHE PATH "")
+set(CMAKE_C_COMPILER ${COMPILER_DIR}/bin/gcc CACHE PATH "")
+set(CMAKE_CXX_COMPILER ${COMPILER_DIR}/bin/g++ CACHE PATH "")
 
 # C++ options
 set(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG -mcpu=power9 -mtune=power9" CACHE STRING "")
