23cab5cb4386c075d55099d3fc59d7b16af64826
Brian Han <han12@llnl.gov>
2023-09-13 10:42:14 -0700

    Test path to local lassen toss 4 installations

--- host-configs/LLNL/llnl-tpls-base.cmake
+++ host-configs/LLNL/llnl-tpls-base.cmake
@@ -1,2 +1,5 @@
-set(GEOSX_TPL_ROOT_DIR /usr/gapps/GEOSX/thirdPartyLibs CACHE PATH "")
-set(GEOSX_TPL_DIR ${GEOSX_TPL_ROOT_DIR}/2023-09-06/install-${CONFIG_NAME}-release CACHE PATH "")
+#set(GEOSX_TPL_ROOT_DIR /usr/gapps/GEOSX/thirdPartyLibs CACHE PATH "")
+#set(GEOSX_TPL_DIR ${GEOSX_TPL_ROOT_DIR}/2023-09-06/install-${CONFIG_NAME}-release CACHE PATH "")
+
+# Temporary path for testing
+set(GEOSX_TPL_DIR /usr/workspace/han12/thirdPartyLibs/install-${CONFIG_NAME}-release CACHE PATH "")
