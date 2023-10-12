8e57b7da0f5f1ecac0c1b85fef53d233dda5e5c7
wrtobin <tobin6@llnl.gov>
2023-10-12 15:52:24 -0700

    tioga host-config changes, may be temp

--- host-configs/LLNL/llnl-tpls-base.cmake
+++ host-configs/LLNL/llnl-tpls-base.cmake
@@ -1,2 +1,3 @@
-set(GEOSX_TPL_ROOT_DIR /usr/gapps/GEOSX/thirdPartyLibs CACHE PATH "")
-set(GEOSX_TPL_DIR ${GEOSX_TPL_ROOT_DIR}/2023-09-06/install-${CONFIG_NAME}-release CACHE PATH "")
+#set(GEOSX_TPL_ROOT_DIR /usr/gapps/GEOSX/thirdPartyLibs CACHE PATH "")
+#set(GEOSX_TPL_DIR ${GEOSX_TPL_ROOT_DIR}/2023-09-06/install-${CONFIG_NAME}-release CACHE PATH "")
+set( GEOSX_TPL_DIR /usr/WS1/tobin6/geosx/thirdPartyLibs/hdf5-cmake/install-${CONFIG_NAME}-release CACHE PATH "" )
--- host-configs/LLNL/tioga-cce-15.cmake
+++ host-configs/LLNL/tioga-cce-15.cmake
@@ -1,22 +1,22 @@
 
-set(CONFIG_NAME "tioga-cce@15.0.0c" CACHE PATH "")
+set(CONFIG_NAME "tioga-cce@15.0.0a" CACHE PATH "")
 include( ${CMAKE_CURRENT_LIST_DIR}/tioga-base.cmake )
 
 # Set up the tpls
 set(TPL_INSTALL_DATE 2023-05-17)
 set(GEOSX_TPL_DIR "/usr/WS1/GEOS/GEOSX/TPLs_${TPL_INSTALL_DATE}/install-${CONFIG_NAME}" CACHE PATH "")
 
-set(CAMP_DIR "${GEOSX_TPL_DIR}/camp-main" CACHE PATH "" )
-set(RAJA_DIR "${GEOSX_TPL_DIR}/raja-develop" CACHE PATH "" )
+set(CAMP_DIR "${GEOSX_TPL_DIR}/camp" CACHE PATH "" )
+set(RAJA_DIR "${GEOSX_TPL_DIR}/raja" CACHE PATH "" )
 
 set(ENABLE_UMPIRE TRUE CACHE BOOL "" )
-set(UMPIRE_DIR "${GEOSX_TPL_DIR}/umpire-develop" CACHE PATH "" )
+set(UMPIRE_DIR "${GEOSX_TPL_DIR}/umpire" CACHE PATH "" )
 
 set(ENABLE_CHAI TRUE CACHE BOOL "" )
-set(CHAI_DIR "${GEOSX_TPL_DIR}/chai-develop" CACHE PATH "" )
+set(CHAI_DIR "${GEOSX_TPL_DIR}/chai" CACHE PATH "" )
 
-set(METIS_DIR "${GEOSX_TPL_DIR}/metis-5.1.0" CACHE PATH "" )
-set(PARMETIS_DIR "${GEOSX_TPL_DIR}/parmetis-4.0.3" CACHE PATH "" )
+set(METIS_DIR "${GEOSX_TPL_DIR}/metis" CACHE PATH "" )
+set(PARMETIS_DIR "${GEOSX_TPL_DIR}/parmetis" CACHE PATH "" )
 
 # C++ options
 set(CRAYPE_VERSION "2.7.19")
