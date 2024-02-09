6867a155da8f5fb31738f384c27cf58aea123e19
Randolph R. Settgast <settgast1@llnl.gov>
2024-02-09 12:27:32 -0800

    guard compiler warning pragmas

--- src/arrayManipulation.hpp
+++ src/arrayManipulation.hpp
@@ -298,11 +298,15 @@ void resize( T * const LVARRAY_RESTRICT ptr,
     if( newSize - size > 0 )
     {
       std::size_t const sizeDiff = integerConversion< std::size_t >( newSize - size );
+#if __GNUC__
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Warray-bounds"
 #pragma GCC diagnostic ignored "-Wstringop-overflow="
+#endif
       memset( reinterpret_cast< void * >( ptr + size ), 0, ( sizeDiff ) * sizeof( T ) );
+#if __GNUC__
 #pragma GCC diagnostic pop
+#endif
     }
   }
   else
