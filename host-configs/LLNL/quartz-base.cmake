cb04b7b34393c777de4ace4eeabcc4acecdc5793
Jay Niles Appleton <appleton@quartz2300.llnl.gov>
2024-02-28 12:01:17 -0800

    removed bkp config clutter

--- host-configs/LLNL/quartz-base.cmake
+++ host-configs/LLNL/quartz-base.cmake
@@ -1,8 +1,5 @@
 set(ENABLE_FORTRAN OFF CACHE BOOL "")
 
-#set(GEOSX_TPL_ROOT_DIR /usr/gapps/GEOSX/thirdPartyLibs CACHE PATH "")
-#set(GEOSX_TPL_DIR ${GEOSX_TPL_ROOT_DIR}/2023-05-01/install-${CONFIG_NAME}-release CACHE PATH "")
-
 set(GEOSX_TPL_ROOT_DIR /usr/workspace/appleton/PSAAPSpace/GEOS CACHE PATH "")
 set(GEOSX_TPL_DIR ${GEOSX_TPL_ROOT_DIR}/install-${CONFIG_NAME}-release CACHE PATH "")
 
