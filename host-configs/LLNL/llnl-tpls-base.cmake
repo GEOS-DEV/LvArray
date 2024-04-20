fb5200d743621126f2117eaef0dd855792fa0443
Matteo Cusini <49037133+CusiniM@users.noreply.github.com>
2024-04-20 15:12:38 -0700

    update tpls path on LC to 2024-04-19 (#320)

--- host-configs/LLNL/llnl-tpls-base.cmake
+++ host-configs/LLNL/llnl-tpls-base.cmake
@@ -1,2 +1,2 @@
 set(GEOSX_TPL_ROOT_DIR /usr/gapps/GEOSX/thirdPartyLibs CACHE PATH "")
-set(GEOSX_TPL_DIR ${GEOSX_TPL_ROOT_DIR}/2024-02-02/install-${CONFIG_NAME}-release CACHE PATH "")
+set(GEOSX_TPL_DIR ${GEOSX_TPL_ROOT_DIR}/2024-04-19/install-${CONFIG_NAME}-release CACHE PATH "")
