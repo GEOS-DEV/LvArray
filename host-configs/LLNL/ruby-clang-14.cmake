84e634d2698eba739e2ba82a26e2b9081489c0ab
Matteo Cusini <matteo.cusini@gmail.com>
2024-06-25 07:23:46 -0700

    unify compiler settings.

--- host-configs/LLNL/ruby-clang-14.cmake
+++ host-configs/LLNL/ruby-clang-14.cmake
@@ -1,13 +1,3 @@
 set(CONFIG_NAME "ruby-clang-14" CACHE PATH "")
 
-set(COMPILER_DIR /usr/tce/packages/clang/clang-14.0.6-magic)
-
-# C
-set(CMAKE_C_COMPILER ${COMPILER_DIR}/bin/clang CACHE PATH "")
-set(CMAKE_C_FLAGS_RELEASE "-O3 -DNDEBUG -march=native -mtune=native" CACHE STRING "")
-
-# C++
-set(CMAKE_CXX_COMPILER ${COMPILER_DIR}/bin/clang++ CACHE PATH "")
-set(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG -march=native -mtune=native" CACHE STRING "")
-
-include(${CMAKE_CURRENT_LIST_DIR}/llnl-cpu-base.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/llnl-cpu-clang-14.cmake)
