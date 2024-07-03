1d2490252c5cc97f87c9da0f5dd305e66f62f2c2
Victor A. P. Magri <50467563+victorapm@users.noreply.github.com>
2024-07-02 21:16:45 -0400

    New host configs for LLNL machines (#319)
    * Add cuda-12 host-configs for lassen
    
    * Add lassen-gcc-12-cuda-12 host-config
    
    * Add ruby and dane host configs
    
    ---------
    
    Co-authored-by: Matteo Cusini <matteo.cusini@gmail.com>

---
+++ host-configs/LLNL/dane-gcc-12.cmake
@@ -0,0 +1,3 @@
+set(CONFIG_NAME "dane-gcc-12" CACHE PATH "")
+
+include(${CMAKE_CURRENT_LIST_DIR}/llnl-cpu-gcc-12.cmake)
---
+++ host-configs/LLNL/ruby-clang-14.cmake
@@ -0,0 +1,3 @@
+set(CONFIG_NAME "ruby-clang-14" CACHE PATH "")
+
+include(${CMAKE_CURRENT_LIST_DIR}/llnl-cpu-clang-14.cmake)
