2b0fbff84ff10400eb136cca62c88148bbc504a6
Randolph R. Settgast <settgast1@llnl.gov>
2024-08-26 16:56:30 -0700

    update LC tpl path

--- host-configs/LLNL/llnl-tpls-base.cmake
+++ host-configs/LLNL/llnl-tpls-base.cmake
@@ -1,2 +1,2 @@
 set(GEOS_TPL_ROOT_DIR /usr/gapps/GEOSX/thirdPartyLibs CACHE PATH "")
-set(GEOS_TPL_DIR ${GEOS_TPL_ROOT_DIR}/2024-08-02/install-${CONFIG_NAME}-release CACHE PATH "")
+set(GEOS_TPL_DIR ${GEOS_TPL_ROOT_DIR}/2024-08-26/install-${CONFIG_NAME}-release CACHE PATH "")
