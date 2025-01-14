bf73593226fe6b1e9e184c6f5c03096c3dda3dbc
Cameron Mikel Crook <crook5@ruby966.llnl.gov>
2025-01-14 09:38:08 -0800

    Added debugging flags to host configs

--- host-configs/LLNL/lassen-cuda-11-base.cmake
+++ host-configs/LLNL/lassen-cuda-11-base.cmake
@@ -10,7 +10,7 @@ set(CMAKE_CUDA_ARCHITECTURES 70 CACHE STRING "")
 set(CMAKE_CUDA_STANDARD 17 CACHE STRING "")
 set(CMAKE_CUDA_FLAGS "-restrict -arch ${CUDA_ARCH} --expt-extended-lambda -Werror cross-execution-space-call,reorder,deprecated-declarations" CACHE STRING "")
 set(CMAKE_CUDA_FLAGS_RELEASE "-O3 -DNDEBUG -Xcompiler -DNDEBUG -Xcompiler -O3 -Xcompiler -mcpu=powerpc64le -Xcompiler -mtune=powerpc64le" CACHE STRING "")
-set(CMAKE_CUDA_FLAGS_RELWITHDEBINFO "-g -lineinfo ${CMAKE_CUDA_FLAGS_RELEASE}" CACHE STRING "")
+set(CMAKE_CUDA_FLAGS_RELWITHDEBINFO "-g -lineinfo --source-in-ptx ${CMAKE_CUDA_FLAGS_RELEASE}" CACHE STRING "")
 set(CMAKE_CUDA_FLAGS_DEBUG "-g -G -O0 -Xcompiler -O0" CACHE STRING "")
 
 set(CHAI_CUDA_FLAGS "-arch ${CUDA_ARCH}" CACHE STRING "" FORCE)
