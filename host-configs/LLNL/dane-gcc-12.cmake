2426c77547834d3a464df0eb79757ca981e0af06
jay-a <jay.appleton@colorado.edu>
2025-03-01 19:20:38 -0800

    updated src files to match develop and fixed incorrect LLNL TPL_DIR path

--- host-configs/LLNL/dane-gcc-12.cmake
+++ host-configs/LLNL/dane-gcc-12.cmake
@@ -1,3 +1,14 @@
 set(CONFIG_NAME "dane-gcc-12" CACHE PATH "")
 
+# Set up the tpls
+set( TPL_INSTALL_DATE 2024-12-06 )
+string(TOLOWER "${CMAKE_BUILD_TYPE}" CMAKE_BUILD_TYPE_LOWER)
+set( GEOS_TPL_ROOT_DIR /usr/gapps/GEOSX/thirdPartyLibs CACHE PATH "" )
+set( GEOS_TPL_DIR "${GEOS_TPL_ROOT_DIR}/${TPL_INSTALL_DATE}/install-${CONFIG_NAME}-${CMAKE_BUILD_TYPE_LOWER}" CACHE PATH "")
+
+message( STATUS " " )
+message( STATUS "GEOS_TPL_DIR: ${GEOS_TPL_DIR}" )
+message( STATUS " " )
+
 include(${CMAKE_CURRENT_LIST_DIR}/llnl-cpu-gcc-12.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/dane-base.cmake)
--- src/Macros.hpp
+++ src/Macros.hpp
@@ -66,8 +66,6 @@
  */
 #define STRINGIZE( A ) STRINGIZE_NX( A )
 
-//#pragma message "LVARRAY_DEVICE_COMPILE: " STRINGIZE(LVARRAY_DEVICE_COMPILE)
-
 /**
  * @brief Mark @p X as an unused argument, used to silence compiler warnings.
  * @param X the unused argument.
@@ -123,7 +121,11 @@
  */
 
 #if defined(LVARRAY_DEVICE_COMPILE)
-  #if !defined(NDEBUG) || __HIP_DEVICE_COMPILE__ == 1
+//   #if defined(__HIP_DEVICE_COMPILE__)
+// // empty impl to avoid the possibility of printfs in device code
+// //   on AMD, which can cause performance degradation just by being present
+// #define LVARRAY_ERROR_IF( EXP, MSG )
+  #if (!defined(NDEBUG)) || defined(__HIP_DEVICE_COMPILE__)
 #define LVARRAY_ERROR_IF( EXP, MSG ) \
   do \
   { \
@@ -684,7 +686,7 @@ template< typename >
 struct RAJAHelper
 {};
 
-using serialPolicy = RAJA::loop_exec;
+using serialPolicy = RAJA::seq_exec;
 
 template<>
 struct RAJAHelper< serialPolicy >
