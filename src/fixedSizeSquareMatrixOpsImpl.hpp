85136bec9eba8ed35716196595b762ee6493fd5b
Cameron Mikel Crook <crook5@ruby964.llnl.gov>
2025-06-17 15:08:28 -0700

    Fixed incorrect type

--- src/fixedSizeSquareMatrixOpsImpl.hpp
+++ src/fixedSizeSquareMatrixOpsImpl.hpp
@@ -113,7 +113,7 @@ static void polarDecompositionBase( DST_MATRIX && LVARRAY_RESTRICT_REF R,
     scale< M, M >( R, 0.5 );
 
     // Determine how close R is to being orthogonal using L2Norm(R.R^T-I)
-    real64 copyR[M][M] = { { 0.0 } };
+    FloatingPoint copyR[M][M] = { { 0.0 } };
     copy< M, M >( copyR, R);
     Rij_eq_AikBjk< M, M, M >( RRTMinusI, R, copyR );
     addIdentity< M >( RRTMinusI, -1.0 );
